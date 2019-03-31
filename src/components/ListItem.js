b5451766856c78babadbce7fa17db6ee143a5e9f
Will Warren <willwarrenmail@gmail.com>
2019-03-31 18:21:42 -0400

    added due date function

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,22 +6,20 @@ class ListItem extends Component {
       <section>
         <li
           onClick={() => this.props.deleteItem(this.props.item)}
-          className={this.props.item.completed ? 'completed-item' : ''}
-        >
-          {this.props.text}
-          <button
-            className='edit-button'
-            onClick={() => this.props.editItem(this.props.item)}
-          >
-            Edit
-          </button>
-          <button
-            className='today-button'
-            onCLick={() => this.props.changeItem(this.props.item)}
-          >
-            Due Today
-          </button>
+          className={this.props.item.completed ? 'completed-item' : ''}>
+          {this.props.text}, Due Today = {this.props.dueDate.toString()}
         </li>
+        {/* <button
+          className="edit-button"
+          onClick={() => this.props.editItem(this.props.item)}>
+          Edit
+        </button> */}
+
+        <button
+          className="today-button"
+          onClick={() => this.props.editItem(this.props.item)}>
+          Due Today
+        </button>
       </section>
     )
   }
